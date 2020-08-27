23cfb1942fdc3345ba1bc0dc9a45697592676491
michaelschna <maschnabel@est.itsgg.edu.ec>
2020-08-27 15:36:13 -0500

    conexion a la base de datos y creacion de la aplicacion llamada medicos

--- consultorio/settings.py
+++ consultorio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'medicos',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +80,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME':' pruebas',
+        #'USER': 'root',
+        #'PASSWORD': 'root',
+        #'HOST': '127.0.0.1',
+        #'PORT': '3306',
     }
 }
+
 
 
 # Password validation
