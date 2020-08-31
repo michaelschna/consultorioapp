94c08e72e7cc12445f5060997db2946aefb00269
peterreyes <pkreyes@est.itsgg.edu.ec>
2020-08-31 14:00:12 -0500

    actualizacion  del formulario :)

--- medicos/views.py
+++ medicos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .forms import MedicosForm, Medicos
 
 # Create your views here.
@@ -11,14 +11,43 @@ def medico(request, plantilla="medicos.html"):
 
 
 #pagina de crear o insertar INSERT
-def crearmedicos(request, template_name="crearmedicos.html"):
+def crearmedicos(request, plantilla="crearmedicos.html"):
 
     if request.method == "POST":
-        form = MedicosForm(request.POST)
+        form = MedicosForm((request.POST or None))
         if form.is_valid():
             form.save()
-            redirect('medicos')
+            return redirect('medicos')
     else:
         form = MedicosForm
 
-    return render(request, template_name, {'medicos':form})
+    return render(request, plantilla, {'form': form})
+
+#pagina de crear o insertar INSERT
+def modificarmedicos(request, pk, plantilla="modificarmedicos.html"):
+
+    if request.method == "POST":
+        form = MedicosForm((request.POST or None))
+        if form.is_valid():
+            form.save()
+            return redirect('medicos')
+    else:
+        medico = get_object_or_404(Medicos, pk=pk)
+        form = MedicosForm(request.POST or None, instance=medico)
+
+    return render(request, plantilla, {'form': form})
+
+
+def eliminarmedicos(request, pk, plantilla="eliminarmedicos.html"):
+
+    if request.method == "POST":
+        form = MedicosForm((request.POST or None))
+        medico = get_object_or_404(Medicos, pk=pk)
+        if form.is_valid():
+            medico.delete()
+            return redirect('medicos')
+    else:
+        medico = get_object_or_404(Medicos, pk=pk)
+        form = MedicosForm(request.POST or None, instance=medico)
+
+    return render(request, plantilla, {'form': form})
